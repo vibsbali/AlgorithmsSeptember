85a50484e48a6dbd5a5b45aa33a94e56d3f3d44a
Vaibhav Bali <vbali@hub24.com.au>
2018-09-18 13:38:09 +1000

    Incomplete Linked List implementation

---
+++ LinkedList/SinglyLinkedList/SingleLinkedList.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using LinkedList.DoublyLinkedList;
+
+namespace LinkedList.SinglyLinkedList
+{
+    public class SingleLinkedList<T> : ICollection<T>
+        where T : IComparable<T>
+    {
+        private SingleLinkedListNode<T> Head { get; set; }
+        private SingleLinkedListNode<T> Tail { get; set; }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var current = Head;
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Add(T item)
+        {
+            var nodeToAdd = new SingleLinkedListNode<T>(item);
+
+            if (Head == null)
+            {
+                Head = Tail = nodeToAdd;
+            }
+            else
+            {
+                Tail.Next = nodeToAdd;
+                Tail = nodeToAdd;
+            }
+
+            ++Count;
+
+        }
+
+        public void Clear()
+        {
+            Head = Tail = null;
+            Count = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            var current = Head;
+            while (current != null)
+            {
+                if (current.Value.CompareTo(item) == 0)
+                {
+                    return true;
+                }
+                current = current.Next;
+            }
+
+            return false;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count { get; private set; }
+        public bool IsReadOnly { get; }
+    }
+}
---
+++ LinkedList/SinglyLinkedList/SingleLinkedListNode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LinkedList.DoublyLinkedList
+{
+    class SingleLinkedListNode<T>
+        where T : IComparable<T>
+    {
+        public SingleLinkedListNode(T value)
+        {
+            Value = value;
+        }
+
+        public T Value { get;}
+        public SingleLinkedListNode<T> Next { get; set; }
+    }
+}
