7de8234064a9b0a123917c12d563784080324425
Vaibhav Bali <vbali@internode.on.net>
2018-09-19 20:59:31 +1000

    Started Queue

--- StackAndQueues/Queues.cs
+++ StackAndQueues/Queues.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using System;
+
+namespace StackAndQueues
+{
+   public class Queue<T>
+   {
+      public Queue()
+      {
+         
+      }
+
+      public void Enqueue(T value)
+      {
+
+      }
+
+      public T Deque()
+      {
+
+      }
+   }
+}
