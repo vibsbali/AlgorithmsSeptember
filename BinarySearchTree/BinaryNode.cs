49a057da6e6a9b30a19af2f1ba233351cbb9e9c5
Vaibhav Bali <vbali@hub24.com.au>
2018-09-19 13:06:25 +1000

    Created Binary Search Tree

---
+++ BinarySearchTree/BinaryNode.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    public class BinaryNode<T>
+        where T : IComparable<T>
+    {
+        public T Value { get; }
+
+        public BinaryNode(T value)
+        {
+            Value = value;
+        }
+
+        public BinaryNode<T> LeftChild { get; set; }
+        public BinaryNode<T> RightChild { get; set; }
+
+        public static bool operator <(BinaryNode<T> lhs, BinaryNode<T> rhs)
+        {
+            return lhs.Value.CompareTo(rhs.Value) < 0;
+        }
+
+        public static bool operator >(BinaryNode<T> lhs, BinaryNode<T> rhs)
+        {
+            return lhs.Value.CompareTo(rhs.Value) > 0;
+        }
+    }
+}
---
+++ BinarySearchTree/BinaryTree.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BinarySearchTree
+{
+    public class BinaryTree<T> : IEnumerable<T>
+        where T : IComparable<T>
+    {
+        internal BinaryNode<T> Root { get; set; }
+
+        public void Add(T value)
+        {
+            var nodeToAdd = new BinaryNode<T>(value);
+
+            if (Root == null)
+            {
+                Root = nodeToAdd;
+            }
+            else
+            {
+                var current = Root;
+                while (current != null)
+                {
+                    if (nodeToAdd < current)
+                    {
+                        if (current.LeftChild == null)
+                        {
+                            current.LeftChild = nodeToAdd;
+                            break;
+                        }
+
+                        current = current.LeftChild;
+                    }
+                    else
+                    {
+                        if (current.RightChild == null)
+                        {
+                            current.RightChild = nodeToAdd;
+                            break;
+                        }
+
+                        current = current.RightChild;
+                    }
+                }
+            }
+
+            ++Count;
+        }
+
+        public int Count { get; private set; }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var itemsToReturn = new Queue<T>(Count);
+            var auxQueue = new Queue<BinaryNode<T>>(Count);
+
+            auxQueue.Enqueue(Root);
+            while (auxQueue.Count > 0)
+            {
+                var current = auxQueue.Dequeue();
+                if (current != null)
+                {
+                    itemsToReturn.Enqueue(current.Value);
+
+                    auxQueue.Enqueue(current.LeftChild);
+                    auxQueue.Enqueue(current.RightChild);
+                }
+            }
+
+            yield return itemsToReturn.Dequeue();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void InOrderTraversal(Action<T> actionToPerform)
+        {
+            RecursivelyInOrder(Root, actionToPerform);
+        }
+
+        private void RecursivelyInOrder(BinaryNode<T> current, Action<T> actionToPerform)
+        {
+            if (current != null)
+            {
+                RecursivelyPreOrder(current.LeftChild, actionToPerform);
+                actionToPerform(current.Value);
+                RecursivelyPreOrder(current.RightChild, actionToPerform);
+            }
+        }
+
+        public void PreOrderTraversal(Action<T> actionToPerform)
+        {
+            RecursivelyPreOrder(Root, actionToPerform);
+        }
+
+        private void RecursivelyPreOrder(BinaryNode<T> current, Action<T> actionToPerform)
+        {
+            if (current != null)
+            {
+                actionToPerform(current.Value);
+                RecursivelyPreOrder(current.LeftChild, actionToPerform);
+                RecursivelyPreOrder(current.RightChild, actionToPerform);
+            }
+        }
+
+        public void PostOrderTraversal(Action<T> actionToPerform)
+        {
+            RecursivelyPostOrder(Root, actionToPerform);
+        }
+
+        private void RecursivelyPostOrder(BinaryNode<T> current, Action<T> actionToPerform)
+        {
+            if (current != null)
+            {
+                RecursivelyPreOrder(current.LeftChild, actionToPerform);
+                RecursivelyPreOrder(current.RightChild, actionToPerform);
+                actionToPerform(current.Value);
+            }
+        }
+    }
+}
